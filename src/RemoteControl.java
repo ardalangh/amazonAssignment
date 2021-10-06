add42f9a06ada892709f67419e7cb48e7c5eac0e
ardalangh <ardygh@berkeley.edu>
2021-10-05 20:29:26 -0700

    throw err if needed

--- src/RemoteControl.java
+++ src/RemoteControl.java
@@ -13,9 +13,27 @@ public class RemoteControl {
         mapCommandNameToKey.put('T', KivaCommand.TAKE);
 
         for (int i = 0; i < userInput.length(); i++) {
-            res[i] = mapCommandNameToKey.get(userInput.charAt(i));
+            KivaCommand mapperRes = mapCommandNameToKey.get(userInput.charAt(i));
+            if (mapperRes == null) {
+                throw new IllegalArgumentException("Character" + userInput.charAt(i) + " does not correspond to a command!");
+            } else {
+                res[i] = mapperRes;
+            }
+
+
         }
         return res;
+    }
+
+    public static void main(String[] args) {
+        HashMap<Character, KivaCommand> mapCommandNameToKey = new HashMap<>();
+        mapCommandNameToKey.put('F', KivaCommand.FORWARD);
+        mapCommandNameToKey.put('D', KivaCommand.DROP);
+        mapCommandNameToKey.put('L', KivaCommand.TURN_LEFT);
+        mapCommandNameToKey.put('R', KivaCommand.TURN_RIGHT);
+        mapCommandNameToKey.put('T', KivaCommand.TAKE);
+
+
     }
 
 }
